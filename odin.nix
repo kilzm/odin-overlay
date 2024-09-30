54c6ab8204cd7079e4da322e4f496493bb75d90b
seanakdh <129484371+seanakdh@users.noreply.github.com>
2024-09-30 21:54:52 +0000

    Update odin.nix

--- odin.nix
+++ odin.nix
@@ -39,9 +39,9 @@ in stdenv.mkDerivation {
       --prefix PATH : ${
         lib.makeBinPath (with llvmPackages; [ bintools llvm clang lld ])
       } \
-      --prefix LD_LIBRARY_PATH : ${
-          lib.makeLibraryPath (with pkgs; [ xorg.libX11 libGL])
-        } \ 
+      #--prefix LD_LIBRARY_PATH : ${
+      #    lib.makeLibraryPath (with pkgs; [ xorg.libX11 libGL])
+      #  } \ 
       --set-default ODIN_ROOT $out/share
 
     runHook postInstall
